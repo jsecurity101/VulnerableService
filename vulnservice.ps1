d1b2223a1b9f1019fbef83cbc895a87085cee1f6
Jonathan Johnson <johnson.s.jonathan@gmail.com>
2019-08-04 17:04:26 -0700

    Update vulnservice.ps1

--- vulnservice.ps1
+++ vulnservice.ps1
@@ -1,4 +1,4 @@
-New-Service -Name VulnerableService -BinaryPathName C:\vulernableservice\vulnservice.exe -Description vulnservice -DisplayName vulnservice -StartupType Automatic
-cmd /c "icacls C:\vulernableservice\vulnservice.exe /t /grant Everyone:(OI)(CI)F"
-cmd /c "sc sdset vulnerableservice D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;WD)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)"
+New-Service -Name VulnerableService -BinaryPathName C:\vulnerableservice\vulnservice.exe -Description VulnerableService -DisplayName VulnerableService -StartupType Automatic
+cmd /c "icacls C:\vulnerableservice\vulnservice.exe /t /grant Everyone:(OI)(CI)F"
+cmd /c "sc sdset VulnerableService D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;WD)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)"
 start-service VulnerableService
